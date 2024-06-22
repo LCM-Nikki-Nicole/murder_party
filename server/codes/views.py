192d771e8f7d49689cb7728ceb16c1cae513a75a
noliver-ykc <74501096+noliver-ykc@users.noreply.github.com>
2024-06-22 21:58:38 +0900

    Revert "Logged in Character screen"

--- server/codes/views.py
+++ server/codes/views.py
@@ -2,10 +2,6 @@ from config import PUZZLE_CODE
 from django.views.generic.edit import FormView
 from django.http import HttpResponse
 from .forms import CodeForm
-# Nicole doesnt know what she's doing but she'S TRYING
-from django.http import JsonResponse
-from django.views.decorators.csrf import csrf_exempt
-import json
 
 class CodeInputView(FormView):
     template_name = 'code.html'
@@ -21,14 +17,3 @@ class CodeInputView(FormView):
         else:
             form.add_error('code', 'The code is incorrect.')
             return self.form_invalid(form)
-
-# AM I DOING IT NOW MR.KRABS
-@csrf_exempt
-def validate_code(request):
-    if request.method == "POST":
-        data = json.loads(request.body)
-        code = data.get("code")
-        if code == PUZZLE_CODE:
-            return JsonResponse({"valid": True})
-        return JsonResponse({"valid": False})
-    return JsonResponse({"valid": False})
